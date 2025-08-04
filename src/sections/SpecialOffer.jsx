6506d95484fc3a5c6d723bfc288f9bf5f2118843
Letera Tujo Birri <168364436+letera1@users.noreply.github.com>
2025-08-04 22:58:19 +0300

    Update SpecialOffer.jsx

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -25,7 +25,7 @@ const SpecialOffer = () => {
           offer unparalleled value that sets us apart.
         </p>
         <p className='mt-6 info-text'>
-          Navigate a realm of possibilities designed to fulfill your unique
+          Navigate a Realm of possibilities designed to fulfill your unique
           desires, surpassing the loftiest expectations. Your journey with us is
           nothing short of exceptional.
         </p>
