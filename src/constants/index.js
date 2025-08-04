96c877916c0a45c6b802de79a0102ad7c635b1ee
Letera Tujo Birri <168364436+letera1@users.noreply.github.com>
2025-08-04 22:56:03 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -31,22 +31,22 @@ export const statistics = [
 
 export const products = [
     {
-        imgURL: shoe4,
+        imgURL:shoe4,
         name: "Nike Air Jordan-01",
         price: "$200.20",
     },
     {
-        imgURL: shoe5,
+        imgURL:shoe5,
         name: "Nike Air Jordan-10",
         price: "$210.20",
     },
     {
-        imgURL: shoe6,
+        imgURL:shoe6,
         name: "Nike Air Jordan-100",
         price: "$220.20",
     },
     {
-        imgURL: shoe7,
+        imgURL:shoe7,
         name: "Nike Air Jordan-001",
         price: "$230.20",
     },
